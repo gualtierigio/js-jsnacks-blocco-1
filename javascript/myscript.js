c41453eb5aea0d3adbad7ae48bc260167c8fd60e
gualtierigio <gualtierigio@gmail.com>
2021-08-27 16:11:19 +0200

    ultimo snack poi pizza

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -44,6 +44,8 @@ document.getElementById("sommaWhile").innerHTML = sommaWhile
  * 
  */
 
+
+/*
  listaNomi = ["Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter"];
  listaCognomi = ["Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler"];
 
@@ -56,4 +58,37 @@ for (let i = 0; i < 8; i++){
 
     document.getElementById("listaInvitati").innerHTML += "<li>" + nomeRandom + " " + cognomeRandom + "</li>";
 }
+*/
 
+
+/**
+ *  A)
+ *   Crea due array che hanno un numero di elementi diversi (di almeno 6 elementi).
+ *   Aggiungi elementi casuali all'array che ha meno elementi, fino a quando ne avrà 
+ *   tanti quanti l'altro.
+ * 
+ *  B)
+ *  Mostriamo in HTML le due liste in parallelo
+ */
+
+ const pizze = ["viennese","capricciosa","prosciutto","tarantina","napoli","cosacca"]
+
+ const pizzeVeg = ["zucchine","cavolfiori"]
+ 
+ for (let i = pizzeVeg.length; i < pizzeVeg.length; i + 1){
+ 
+     let pizzeRandom = pizze[Math.floor(Math.random() * pizze.length)];
+ 
+     pizzeVeg.push(pizzeRandom);
+ }
+ 
+
+
+ while (pizzeVeg.length < pizzeVeg.length){
+
+    let pizzeRandom = pizze[Math.floor(Math.random() * pizze.length)];
+ 
+    pizzeVeg.push(pizzeRandom);
+}
+
+console.log(pizzeVeg)
