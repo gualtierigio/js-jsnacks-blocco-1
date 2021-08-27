b8535699536cd990416e5c0adfd63ae085b26f1f
gualtierigio <gualtierigio@gmail.com>
2021-08-27 14:26:04 +0200

    inizzializzazione snacks

---
+++ javascript/myscript.js
@@ -0,0 +1,17 @@
+/**
+ *  Il software deve chiedere per 5 volte all'utente di inserire un numero.
+ *  Il programma stampa la somma di tutti i numeri inseriti.
+ *  Esegui questo programma in due versioni, con il for e con il while.
+ */
+
+let somma = 0
+
+for (let i = 0; i < 5; i++){
+    
+    numero = parseInt(prompt("Inserisci un numero per 5 volte"));
+
+    somma = somma + numero;
+
+}
+
+console.log(somma)
